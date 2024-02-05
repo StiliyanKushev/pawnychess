3295b4e39d8a68c43d1b20dc358c6a6843c4c60c
StiliyanKushev <stiliyankushev@gmail.com>
2024-02-05 17:51:52 +0200

    feat: add pino pretty logger

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { LoggerModule } from 'nestjs-pino';
 import { RedisModule } from 'redis/redis.module';
 import { DatabaseModule } from './database/database.module';
 import { IamModule } from './iam/iam.module';
@@ -8,6 +9,25 @@ import { ValidationModule } from './validation/validation.module';
 
 @Module({
   imports: [
+    LoggerModule.forRoot({
+      pinoHttp: {
+        customProps: () => ({
+          context: 'HTTP',
+        }),
+        transport: {
+          target: 'pino-pretty',
+          options: {
+            singleLine: true,
+            messageFormat:
+              '[{context}]{if req.method}:' +
+              ' [{req.remoteAddress}] [{req.method}] [{req.url}]' +
+              ' | [{res.statusCode}] - [{responseTime}ms]' +
+              '{end}: {msg}',
+            ignore: 'pid,hostname,context,req,res,responseTime',
+          },
+        },
+      },
+    }),
     ConfigModule.forRoot(),
     DatabaseModule.forRoot(),
     RedisModule.forRoot(),
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
+import { Logger, LoggerErrorInterceptor } from 'nestjs-pino';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { bufferLogs: true });
+  app.useLogger(app.get(Logger));
+  app.useGlobalInterceptors(new LoggerErrorInterceptor());
   await app.listen(3000);
 }
 bootstrap();
