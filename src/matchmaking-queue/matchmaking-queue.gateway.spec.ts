11161701290a907f88638d988fa17757971691c8
StiliyanKushev <stiliyankushev@gmail.com>
2024-02-11 17:55:56 +0200

    chore: define empty providers in mm queue gateway test

--- src/matchmaking-queue/matchmaking-queue.gateway.spec.ts
+++ src/matchmaking-queue/matchmaking-queue.gateway.spec.ts
@@ -1,12 +1,24 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { AccessTokenGuard } from 'iam/authentication/guards/access-token/access-token.guard';
 import { MatchmakingQueueGateway } from './matchmaking-queue.gateway';
+import { MatchmakingQueueService } from './matchmaking-queue.service';
 
 describe('MatchmakingQueueGateway', () => {
   let gateway: MatchmakingQueueGateway;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [MatchmakingQueueGateway],
+      providers: [
+        MatchmakingQueueGateway,
+        {
+          provide: MatchmakingQueueService,
+          useValue: {},
+        },
+        {
+          provide: AccessTokenGuard,
+          useValue: {},
+        },
+      ],
     }).compile();
 
     gateway = module.get<MatchmakingQueueGateway>(MatchmakingQueueGateway);
